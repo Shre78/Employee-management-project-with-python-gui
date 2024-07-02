1c0d507041ab738c0fb8efe1ab2e2856fc9efaec
Shre78 <150413055+Shre78@users.noreply.github.com>
2024-07-02 14:05:08 +0530

    Update emp.sql

--- emp.sql
+++ emp.sql
@@ -14,10 +14,10 @@ CREATE TABLE employees (
 );
 
 -- Insert Sample Data
-INSERT INTO employees (name, age, job profile , salary) VALUES ('RAHUL’, 30, 'HR', 50000);
-INSERT INTO employees (name, age, job profile, salary) VALUES ('SHREYASH, 21, 'Software developer, 60000);
-INSERT INTO employees (name, age, job profile , salary) VALUES ('PRAVEEN’, 21, 'Data analytics', 70000);
-INSERT INTO employees (name, age, job profile , salary) VALUES ('PRASHANT', 21, 'Cyber security', 55000);
+INSERT INTO employees (name, age, jobprofile, salary) VALUES ('rahul', 30, 'HR', 50000);
+INSERT INTO employees (name, age, jobprofile, salary) VALUES ('shreyash', 21, 'software engineer', 60000);
+INSERT INTO employees (name, age, jobprofile, salary) VALUES ('praveen', 21, 'IT', 70000);
+INSERT INTO employees (name, age, jobprofile, salary) VALUES ('prashant', 21, 'software engineer', 55000);
 
 -- Verify the inserted data
 SELECT * FROM employees;
